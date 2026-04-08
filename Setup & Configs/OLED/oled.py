51a82d41f97993d4ba7274dd42b05d36199aa1ee
Ștefan Emil - Antoniu <76394495+emil-antoniu@users.noreply.github.com>
2026-04-08 17:10:41 +0300

    Update oled.py script

--- Setup & Configs/OLED/oled.py
+++ Setup & Configs/OLED/oled.py
@@ -6,15 +6,13 @@ import time
 from adafruit_ssd1306 import SSD1306_I2C
 from PIL import Image, ImageDraw, ImageFont
 
-def is_vpn_connected():
+def vpn_status():
 
-        verdict = subprocess.run(
-                ["expressvpn", "status"], capture_output=True, text=True
+        status = subprocess.run(
+                ["expressvpnctl", "status"], capture_output=True, text=True
         )
 
-        if "connected to" in verdict.stdout.lower(): return True
-
-        return False
+        return status.stdout.split()[0].lower()
 
 def can_reach_internet(host="8.8.8.8"):
 
@@ -27,16 +25,13 @@ def can_reach_internet(host="8.8.8.8"):
 
         except any_exception: return False
 
-def is_p2p_running(service="qbittorrent-nox.service"):
+def ufw_status():
 
-        try:
-                verdict = subprocess.run(
-                        ["systemctl", "is-active", service], capture_output=True, text=True
-                )
+        status = subprocess.run(
+                ["sudo", "ufw", "status"], capture_output=True, text=True
+        )
 
-                return verdict.stdout.strip() == "active"
-
-        except any_exception: return False
+        return status.stdout.split(" ")[-1]
 
 
 def fileserver_uptime():
@@ -51,7 +46,7 @@ def fileserver_uptime():
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # Create the OLED class
-oled = SSD1306_I2C(128, 64, i2c)  # adjust if you have a 128x32 display
+oled = SSD1306_I2C(128, 64, i2c)
 
 # Load default font
 font = ImageFont.load_default()
@@ -64,19 +59,12 @@ while True:
 
         # Draw text
         if can_reach_internet() == True:
-                draw.text((0, 0), "WAN.............................OK", font=font, fill=255)
+                draw.text((0, 0), "WAN: ok", font=font, fill=255)
         else:
-                draw.text((0, 0), "WAN........................ERROR", font=font, fill=255)
+                draw.text((0, 0), "WAN: failed ping", font=font, fill=255)
+        draw.text((0, 0), "\nVPN: {0}".format(vpn_status()), font=font, fill=255)
 
-        if is_vpn_connected() == True:
-                draw.text((0, 0), "\nVPN..............................OK", font=font, fill=255)
-        else:
-                draw.text((0, 0), "\nVPN........................ERROR", font=font, fill=255)
-
-        if is_p2p_running() == True:
-                draw.text((0, 0), "\n\nP2P..............................OK", font=font, fill=255)
-        else:
-                draw.text((0, 0), "\n\nP2P........................ERROR", font=font, fill=255)
+        draw.text((0, 0), "\n\nUFW: {0}".format(ufw_status()), font=font, fill=255)
 
         draw.text((0, 0), "\n\n\n{0}".format(fileserver_uptime()), font=font, fill=255)
 
