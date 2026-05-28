ff5a3353459d49374ece9e3e635c0faa3531e22a
Ștefan Emil - Antoniu <76394495+emil-antoniu@users.noreply.github.com>
2026-05-28 07:58:34 +0000

    Document lscpu usage

--- misc/demos/device_management/dev_mgmt_demo.sh
+++ misc/demos/device_management/dev_mgmt_demo.sh
@@ -1,6 +1,6 @@
 echo "The ring buffer is a cyclic data structure that has a fixed size, and is hard-coded into the kernel. Users can display data stored in the kernel ring buffer through the dmesg command or the /var/log/boot>
 echo -e -n "\n"
-read -r  -p "Hit ENTER for a view into dmesg output."
+read -r -p "Hit ENTER for a view into dmesg output."
 echo -e -n "\n"
 dmesg | tail
 echo -e -n "\n\n"
@@ -14,4 +14,11 @@ read -r -p "Next up: IPMI. My Pi lacks a BMC(2) which is needed for IPMI to work
 echo -e -n "\n"
 # ipmitool needs to be installed
 man ipmitool | head --lines=43 | tail --lines=3
+echo -e -n "\n\n"
+
+read -r -p "Next up: lscpu."
+echo -e -n "\n"
+man lscpu | head --lines=12 | tail --lines=3
+read -r -p "Hit ENTER for a view into lscpu output."
+lscpu
 echo -e -n "\n\n"
