e09cddba54b61e15ff0993a6848c253096fe5e6e
Ștefan Emil - Antoniu <76394495+emil-antoniu@users.noreply.github.com>
2026-06-02 16:15:23 +0000

    Overview of commands to document

---
+++ misc/demos/kernel_modules/kernel_mod_demo.sh
@@ -0,0 +1,11 @@
+# read -r -p "Next up: depmod"
+
+# read -r -p "Next up: insmod"
+
+# read -r -p "Next up: lsmod"
+
+# read -r -p "Next up: modinfo"
+
+# read -r -p "Next up: modprobe"
+
+# read -r -p "Next up: rmmod"
