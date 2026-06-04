39802848321b8b25177c2f079508516a9fddb2b3
Ștefan Emil - Antoniu <76394495+emil-antoniu@users.noreply.github.com>
2026-06-04 10:50:12 +0000

    Document depmod

--- misc/demos/kernel_modules/kernel_mod_demo.sh
+++ misc/demos/kernel_modules/kernel_mod_demo.sh
@@ -1,4 +1,10 @@
-# read -r -p "Next up: depmod"
+read -r -p "First command we will look at is: depmod"
+echo -e -n "\n"
+man depmod | head --lines=22 | tail --lines=6
+read -r -p "Hit ENTER for a view into depmod output."
+echo -e -n "\n"
+depmod --verbose | head
+echo -e -n "\n\n"
 
 # read -r -p "Next up: insmod"
 
