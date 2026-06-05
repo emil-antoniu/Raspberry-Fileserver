e8302e8c6be0dd9e9c33e12c47937e98c51170d1
Ștefan Emil - Antoniu <76394495+emil-antoniu@users.noreply.github.com>
2026-06-05 11:01:02 +0000

    Document modinfo

--- misc/demos/kernel_modules/kernel_mod_demo.sh
+++ misc/demos/kernel_modules/kernel_mod_demo.sh
@@ -15,7 +15,12 @@ read -r -p "Hit ENTER for a view into lsmod output."
 lsmod | head
 echo -e -n "\n\n"
 
-# read -r -p "Next up: modinfo"
+read -r -p "Next up: modinfo"
+echo -e -n "\n"
+man modinfo | head --lines=15 | tail --lines=2
+read -r -p "Hit ENTER for a view into modinfo output (targeting the module xt_nat as an example)."
+modinfo xt_nat
+echo -e -n "\n\n"
 
 # read -r -p "Next up: modprobe"
 
