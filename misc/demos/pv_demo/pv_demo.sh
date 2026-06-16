4a99cfdc1d92bb8300867869065d09f9581646c2
Ștefan Emil - Antoniu <76394495+emil-antoniu@users.noreply.github.com>
2026-06-16 16:00:12 +0000

    Documenting pvcreate...

--- misc/demos/pv_demo/pv_demo.sh
+++ misc/demos/pv_demo/pv_demo.sh
@@ -1,4 +1,12 @@
-# pvcreate
+# lvm2 package is needed
+# sudo apt install lvm2
+read -r -p "First command we will look at is: pvcreate"
+echo -e -n "\n"
+man pvcreate | head --lines=22 | tail --lines=6
+read -r -p "Hit ENTER for..."
+echo -e -n "\n"
+
+echo -e -n "\n\n"
 # pvdisplay
 # pvmove
 # pvremove
